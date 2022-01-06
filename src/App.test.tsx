51a88ca423cd7346dd2d4d3ed6a26e943293948b
iamyuu <akuyusuf27@gmail.com>
2022-01-06 23:08:04 +0700

    feat: setup vitest and add example test

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react"
+import App from "./App"
+
+test("should be show the title", () => {
+	render(<App />)
+
+	// https://github.com/testing-library/jest-dom#tobeinthedocument
+	expect(screen.getByText(/Hello Vite \+ React!/i)).toBeInTheDocument()
+})
